642c67c344e4238e4fc7cc4b5986605796011b68
larioteo <larioteo@gmail.com>
2024-01-04 07:45:26 +0100

    Readd stb

---
+++ 3rd-Party/stb/stb.lua
@@ -0,0 +1,14 @@
+﻿project "stb"
+	kind "StaticLib"
+	language "C"
+    conformancemode "true"
+    cdialect "C17"
+    staticruntime "on"
+    toolset "msc"
+    warnings "Default"
+
+	files {
+        "include/**",
+		"source/**",
+        "stb.lua",
+	}
