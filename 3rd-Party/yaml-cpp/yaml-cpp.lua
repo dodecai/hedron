4e152d2f0b7941095eec8d92bec2b51415909399
larioteo <larioteo@gmail.com>
2024-01-04 08:03:10 +0100

    Readd yaml-cpp

---
+++ 3rd-Party/yaml-cpp/yaml-cpp.lua
@@ -0,0 +1,44 @@
+﻿project "yaml-cpp"
+	kind "StaticLib"
+	language "C++"
+    characterset "Unicode"
+    conformancemode "true"
+    cdialect "C17"
+	cppdialect "C++20"
+    staticruntime "on"
+    toolset "msc"
+    warnings "Default"
+
+    defines {
+        "YAML_CPP_STATIC_DEFINE",
+        "YAML_CPP_NO_CONTRIB",
+    }
+
+	files {
+        "include/**",
+        "source/**",
+        "yaml-cpp.lua",
+	}
+    
+    includedirs {
+        "include",
+    }
+
+    filter { "configurations:Debug" }
+        defines { "_DEBUG" }
+		runtime "Debug"
+        symbols "On"
+
+    filter { "configurations:Release" }
+        defines { "NDEBUG" }
+		runtime "Release"
+        optimize "On"
+        symbols "Off"
+
+    filter { "configurations:Distribution" }
+        defines { "NDEBUG" }
+        optimize "On"
+        symbols "Off"
+
+	filter "system:windows"
+		systemversion "latest"
