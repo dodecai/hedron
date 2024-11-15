8ee647fbaf65f577b4d4f1e232dad16a80834286
larioteo <larioteo@gmail.com>
2024-11-15 03:07:01 +0100

    Update Scripts

--- Scripts/GitListLargestFilesInHistory.ps1
+++ Scripts/GitListLargestFilesInHistory.ps1
@@ -5,16 +5,25 @@
   $batchInput = $objects | ForEach-Object { ($_ -split ' ')[0] } | Out-String
   $sizeOutput = $batchInput | git cat-file --batch-check | Out-String -Stream
 
+# Define the extensions to ignore
+  $ignoredExtensions = @('h', 'hpp', 'cpp', 'ixx', 'cxx')
+
 # Combine path, hash, size and sort them by the larges
   $combined = $objects | ForEach-Object -Begin {
     $index = 0
   } -Process {
     $line = $_ -split ' '
     $path = $line[1]
-    $sizeInfo = $sizeOutput[$index] -split ' '
-    $hash = $sizeInfo[0]
-    $size = $sizeInfo[2] -as [int]
-    [PSCustomObject]@{ Hash = $hash; Path = $path; Size = $size }
+    
+    # Check if the file extension should be ignored
+    $extension = [System.IO.Path]::GetExtension($path).TrimStart('.').ToLower()
+      if ($ignoredExtensions -notcontains $extension) {
+      $sizeInfo = $sizeOutput[$index] -split ' '
+      $hash = $sizeInfo[0]
+      $size = $sizeInfo[2] -as [int]
+      [PSCustomObject]@{ Hash = $hash; Path = $path; Size = $size }
+    }
+
     $index++
   } | Sort-Object Size -Descending | Select-Object -First 32
 
